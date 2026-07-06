f8735da88385f062ad41f427a8615c9032f480c0
jydemagt <67008158+jydemagt@users.noreply.github.com>
2026-07-06 13:31:58 +0200

    Update Plugin.php

--- includes/Core/Plugin.php
+++ includes/Core/Plugin.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * Main plugin bootstrap class.
- *
- * @package HSGCampaignManager
- */
 
 namespace HSGCM\Core;
 
@@ -18,18 +13,8 @@ defined( 'ABSPATH' ) || exit;
 
 class Plugin {
 
-	/**
-	 * Singleton instance.
-	 *
-	 * @var Plugin|null
-	 */
 	private static ?Plugin $instance = null;
 
-	/**
-	 * Get singleton instance.
-	 *
-	 * @return Plugin
-	 */
 	public static function instance(): Plugin {
 
 		if ( self::$instance === null ) {
@@ -40,80 +25,23 @@ class Plugin {
 
 	}
 
-	/**
-	 * Constructor.
-	 */
 	private function __construct() {
 
-		$this->load();
-
-		add_action( 'plugins_loaded', array( $this, 'init' ), 20 );
+		add_action( 'plugins_loaded', array( $this, 'init' ) );
 
 	}
 
-	/**
-	 * Load plugin core.
-	 *
-	 * @return void
-	 */
-	private function load(): void {
-
-		require_once HSGCM_PATH . 'includes/Core/Loader.php';
-
-		Loader::register();
-
-	}
-
-	/**
-	 * Initialize plugin.
-	 *
-	 * @return void
-	 */
 	public function init(): void {
 
-		// WooCommerce skal være installeret.
 		if ( ! class_exists( 'WooCommerce' ) ) {
-
-			add_action(
-				'admin_notices',
-				function () {
-
-					?>
-					<div class="notice notice-error">
-						<p>
-							<?php esc_html_e( 'HSG Campaign Manager requires WooCommerce to be installed and activated.', 'hsg-campaign-manager' ); ?>
-						</p>
-					</div>
-					<?php
-
-				}
-			);
-
 			return;
-
 		}
 
-		$this->boot_modules();
-
-	}
-
-	/**
-	 * Boot plugin modules.
-	 *
-	 * @return void
-	 */
-	private function boot_modules(): void {
-
 		new Admin();
-
 		new Campaign();
-
 		new Editor();
-
 		new Save();
-
 		new Pricing();
-
 		new Coupons();
 
 	}
