bfe0db3531b6fcee36ddde68db10964d6dee9154
jydemagt <67008158+jydemagt@users.noreply.github.com>
2026-07-06 12:53:42 +0200

    Create Pricing.php

---
+++ includes/Pricing/Pricing.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Pricing Module
+ *
+ * @package HSGCampaignManager
+ */
+
+namespace HSGCM\Pricing;
+
+defined( 'ABSPATH' ) || exit;
+
+class Pricing {
+
+	public function __construct() {
+
+		// Prisberegning kommer i Version 1.3
+
+	}
+
+}
