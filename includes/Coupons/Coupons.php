9d3c17ae3f50442fb70503d0d877cd6b89cc7d3b
jydemagt <67008158+jydemagt@users.noreply.github.com>
2026-07-06 12:53:54 +0200

    Create Coupons.php

---
+++ includes/Coupons/Coupons.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Coupon Module
+ *
+ * @package HSGCampaignManager
+ */
+
+namespace HSGCM\Coupons;
+
+defined( 'ABSPATH' ) || exit;
+
+class Coupons {
+
+	public function __construct() {
+
+		// Kuponmotor kommer senere.
+
+	}
+
+}
